a6f606b9ccc3cd3fbcba81e8e0c44e90567f533f
Hamed Navvabian <hamednn76@MacBook-Pro.local>
2019-08-05 18:52:56 +0430

    remove example directory

--- example/Client/src/Components/index.js
+++
@@ -1,9 +0,0 @@
-import Modal from './Modal';
-import Button from './Button';
-import Input from './Input';
-
-export {
-  Modal,
-  Button,
-  Input
-};
--- example/Server/setHeaders.js
+++
@@ -1,6 +0,0 @@
-function setHeaders(req, res, next) {
-    res.setHeader('Content-Type', 'application/json');
-    next();
-}
-
-module.exports = setHeaders;
